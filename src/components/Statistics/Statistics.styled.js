916e8064ce124674b3e770c3899187e4d72abc02
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-15 10:47:27 +0100

    Create Statistics.styled.js

---
+++ src/components/Statistics/Statistics.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+margin-left: 20px;
+font-size: 30px;
+`
+export const Paragraph = styled.p`
+margin-left: 20px;
+font-weight: 500;
+font-size: 20px;
+
+`
