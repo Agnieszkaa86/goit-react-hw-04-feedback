b8e26f88e2912eb501eb7e7586bed82b04db0480
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-15 10:47:16 +0100

    Create App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = event => {
+    const targetName = event.target.name;
+
+    switch (targetName) {
+    case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        break;
+    }
+  };
+  
+  const total = (good + neutral + bad);
+  const positivePercentage = Math.round((good / total) * 100);
+  const options = ['good', 'neutral', 'bad'];
+  
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={onLeaveFeedback}
+          />
+          {total === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          )}
+        </Section>
+      </>
+    );
+  }
+export default App;
