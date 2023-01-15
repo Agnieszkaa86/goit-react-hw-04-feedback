44b96c900885f803fb2451577d19b3a796d45b41
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-15 10:46:57 +0100

    Create FeedbackOptions.styled.js

---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  font-size: 16px;
+  font-weight: 500;
+  padding: 4px 8px;
+  border: 1px solid rgb(209, 209, 209);
+  border-radius: 5px;
+  text-transform: capitalize;
+  background-color: rgb(255, 255, 255);
+  box-shadow: 0px 0px 4px 0px rgba(0,0,0,0.2);
+  margin-right: 20px;
+  margin-left: 20px;
+`
